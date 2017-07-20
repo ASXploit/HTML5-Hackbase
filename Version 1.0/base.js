fd88f675892b9ef424080f0088e349e8b6a8ca7b
Xploit <linkhto@Gmail.com>
2017-07-19 20:42:16 -0700

    Create base.js

---
+++ Version 1.0/base.js
@@ -0,0 +1,32 @@
+/* Simple Userscript Base;
+ * Created by Xploit ~> @GuidedHacking
+ * =====================
+ * Features~
+ * - Self Executing Update Function
+ * - Self Executing Keyhooking
+ * =====================
+ * Yes i know "Only Two Features?"
+ * If you followed my tutorial you
+ * Would know how to make this and
+ * Exactly how its working..
+ * Still cleaner code than most others..
+ */
+(function(me) {
+(function() {
+    document.addEventListener('keyup', function(e) {
+        switch(e.keyCode || e.which) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                return;
+        } // end switch;
+    }); // end event;
+})(); // end keyHook;
+(function Update() {
+setTimeout(Update, 100);
+})(); // end update;
+})(window); // end;
